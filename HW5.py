b4c99fe32783044c0fa4b183b1a5916852a7357b
Zach <zachjolson@gmail.com>
2025-09-23 13:04:31 -0600

    finished problems 2 and 3

--- HW5.py
+++ HW5.py
@@ -52,23 +52,78 @@ class Three_DOF_Spatial_Manipulator:
         return forward_kinematics 
     
 
-robot = Three_DOF_Spatial_Manipulator()
+arm_robot = Three_DOF_Spatial_Manipulator()
 
-T_zero = robot.FK(0, 0, 0)
-T_theta1_90 = robot.FK(np.pi/2, 0, 0)
-T_theta2_90 = robot.FK(0, np.pi/2, 0)
-T_theta3_90 = robot.FK(0, 0, np.pi/2) 
+T_zero = arm_robot.FK(0, 0, 0)
+T_theta1_90 = arm_robot.FK(np.pi/2, 0, 0)
+T_theta2_90 = arm_robot.FK(0, np.pi/2, 0)
+T_theta3_90 = arm_robot.FK(0, 0, np.pi/2) 
 
-print("0 angle: \n", T_zero)
-print("theta 1 = 90: \n", T_theta1_90)
-print("theta 2 = 90: \n", T_theta2_90)
-print("theta 3 = 90: \n", T_theta3_90)
+# print("0 angle: \n", T_zero)
+# print("theta 1 = 90: \n", T_theta1_90)
+# print("theta 2 = 90: \n", T_theta2_90)
+# print("theta 3 = 90: \n", T_theta3_90)
 
 
 #############################################
 #Problem 2
 #############################################
+class Spherical_Wrist:
+    def __init__(self):
+        self.k1 = np.array([0, 1, 0])
+        self.k2 = np.array([0, 0, 1])
+        self.k3 = np.array([0, 1, 0])
+        self.q1 = self.q2 = self.q3 = np.array([0, 0, 0])
+        self.T_zero_config = np.eye(4)
+    def FK(self, theta1, theta2, theta3):
+        v1 = np.cross(self.q1, self.k1)
+        v2 = np.cross(self.q2, self.k2)
+        v3 = np.cross(self.q3, self.k3)
+        twist3 = twist(v3, self.k3)
+        twist2 = twist(v2, self.k2)
+        twist1 = twist(v1, self.k1)
+
+        forward_kinematics = expm(twist1 * theta1) @ expm(twist2 * theta2) @ expm(twist3 * theta3) @ self.T_zero_config
+
+        return forward_kinematics
+    
+wrist_robot = Spherical_Wrist()
+
+T_wrist_zero = wrist_robot.FK(0, 0, 0)
+T_wrist_theta1_90 = wrist_robot.FK(np.pi/2, 0, 0)
+T_wrist_theta2_90 = wrist_robot.FK(0, np.pi/2, 0)
+T_wrist_theta3_90 = wrist_robot.FK(0, 0, np.pi/2)
+
+# print("0 angle: \n", T_wrist_zero)
+# print("theta 1 = 90: \n", T_wrist_theta1_90)
+# print("theta 2 = 90: \n", T_wrist_theta2_90)
+# print("theta 3 = 90: \n", T_wrist_theta3_90)
 
 
+#############################################
+#Problem 3
+#############################################
+class arm_plus_wrist:
+    def __init__(self, arm_class, wrist_class):
+        self.arm = arm_class
+        self.wrist = wrist_class
+    def FK(self, arm_theta1, arm_theta2, arm_theta3, wrist_theta1, wrist_theta2, wrist_theta3):
+        arm_fk = self.arm.FK(arm_theta1, arm_theta2, arm_theta3)
+        wrist_fk = self.wrist.FK(wrist_theta1, wrist_theta2, wrist_theta3)
+
+        combined_fk = arm_fk @ wrist_fk
+
+        return combined_fk
+    
+robot_6_dof = arm_plus_wrist(arm_robot, wrist_robot)
 
 
+T_6_dof_zero = robot_6_dof.FK(0, 0, 0, 0, 0, 0)
+T_6_dof_theta1_90 = robot_6_dof.FK(np.pi/2, 0, 0, 0, 0, 0)
+T_6_dof_theta3_90 = robot_6_dof.FK(0, 0, np.pi/2, 0, 0, 0)
+T_6_dof_theta4_90 = robot_6_dof.FK(0, 0, 0, np.pi/2, 0, 0)
+
+print("0 angle: \n", T_6_dof_zero)
+print("theta 1 = 90: \n", T_6_dof_theta1_90)
+print("theta 3 = 90: \n", T_6_dof_theta3_90)
+print("theta 4 = 90: \n", T_6_dof_theta4_90)
